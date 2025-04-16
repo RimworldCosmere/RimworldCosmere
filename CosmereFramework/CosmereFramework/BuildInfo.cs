44a14f319e15eaa7b94222af686f0b6c29ef4db1
Aaron Scherer <aequasi@gmail.com>
2025-04-15 23:55:38 -0500

    * Fixing cache on generator scripts * Implementing Tin Burning!
    Signed-off-by: Aaron Scherer <896295+aequasi@users.noreply.github.com>

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string Revision = "a5be73e";
-        
-        public const string BuildTime = "2025-04-16T03:32:13Z";
+        public const string Revision = "9330ef5";
+
+        public const string BuildTime = "2025-04-16T04:53:38Z";
     }
 }

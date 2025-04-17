aa5665f0d39adfc6f84828e2db9c7e5c11a5ae37
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-04-17 12:43:08 -0500

    Adding Brass Aura
    Signed-off-by: Aaron Scherer <896295+aequasi@users.noreply.github.com>

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "e0e4c51";
+        public const string REVISION = "70cc585";
 
-        public const string BUILD_TIME = "2025-04-17T17:25:35Z";
+        public const string BUILD_TIME = "2025-04-17T17:39:18Z";
     }
 }

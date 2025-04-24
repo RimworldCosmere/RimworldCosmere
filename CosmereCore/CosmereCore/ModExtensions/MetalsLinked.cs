5f8b1bc243da839117ab9b4ff9463b7e7b0d64a8
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-04-24 06:05:45 -0500

    Removing MetalLinked ModExtension. Combining with MetalsLinked.cs
    Signed-off-by: Aaron Scherer <896295+aequasi@users.noreply.github.com>

--- CosmereCore/CosmereCore/ModExtensions/MetalsLinked.cs
+++ CosmereCore/CosmereCore/ModExtensions/MetalsLinked.cs
@@ -4,6 +4,9 @@ using Verse;
 
 namespace CosmereCore.ModExtensions {
     public class MetalsLinked : DefModExtension {
+        public MetalDef metal;
         public List<MetalDef> metals;
+
+        public List<MetalDef> Metals => metals ?? (metal != null ? [metal] : []);
     }
 }
--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "32d1285";
+        public const string REVISION = "9c39d4e";
 
-        public const string BUILD_TIME = "2025-04-24T10:38:24Z";
+        public const string BUILD_TIME = "2025-04-24T11:03:07Z";
     }
 }

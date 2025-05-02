a17a1b450101e405d7e03fbdba6cd654acec9304
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-05-02 01:24:26 -0500

    Fixing exports, and readme

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "1bef52a";
+        public const string REVISION = "e0f0db4";
 
-        public const string BUILD_TIME = "2025-05-02T06:13:09Z";
+        public const string BUILD_TIME = "2025-05-02T06:16:55Z";
     }
 }

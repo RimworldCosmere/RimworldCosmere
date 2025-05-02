e0f0db4f65c35ccaf7478193de10df5bee87738c
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-05-02 01:16:49 -0500

    Fixing scadrial build

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "c21adc3";
+        public const string REVISION = "1bef52a";
 
-        public const string BUILD_TIME = "2025-05-02T01:08:59Z";
+        public const string BUILD_TIME = "2025-05-02T06:13:09Z";
     }
 }

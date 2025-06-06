d1e24dc4a161c9589c204173625e7e5bf5eff828
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-06 17:57:26 -0500

    Cleaning up startup

--- CosmereCore/CosmereCore/CosmereCore.cs
+++ CosmereCore/CosmereCore/CosmereCore.cs
@@ -1,8 +1,5 @@
 ﻿using CosmereFramework;
-using HarmonyLib;
 using Verse;
-using static CosmereFramework.CosmereFramework;
-using Log = CosmereFramework.Log;
 
 namespace CosmereCore {
     public class CosmereCore(ModContentPack content) : Mod(content) {
@@ -12,8 +9,7 @@ namespace CosmereCore {
     [StaticConstructorOnStartup]
     public static class ModStartup {
         static ModStartup() {
-            Log.Important($"Build Rev: {BuildInfo.REVISION} @ {BuildInfo.BUILD_TIME}. DebugMode={CosmereSettings.debugMode} LogLevel={CosmereSettings.logLevel}");
-            new Harmony("Cryptiklemur.Cosmere.Core").PatchAll();
+            Startup.Initialize("Cryptiklemur.Cosmere.Core");
         }
     }
 }
---
+++ CosmereFramework/CosmereFramework/Startup.cs
@@ -0,0 +1,11 @@
+using HarmonyLib;
+
+namespace CosmereFramework {
+    public static class Startup {
+        public static void Initialize(string harmonyId) {
+            Log.Important(
+                $"Mod: {harmonyId} Build Rev: {BuildInfo.REVISION} @ {BuildInfo.BUILD_TIME}. DebugMode={CosmereFramework.CosmereSettings.debugMode} LogLevel={CosmereFramework.CosmereSettings.logLevel}");
+            new Harmony(harmonyId).PatchAll();
+        }
+    }
+}

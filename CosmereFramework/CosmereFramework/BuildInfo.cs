a2bf52a296bb3f3d36d9d4aea77a4f45fc968094
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-06 20:33:01 -0500

    Trying to clean up csproj build scripts to allow for better cross-platform development

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "d1e24dc";
+        public const string REVISION = "152696a";
 
-        public const string BUILD_TIME = "2025-06-07T01:23:01Z";
+        public const string BUILD_TIME = "2025-06-07T01:31:22Z";
     }
 }

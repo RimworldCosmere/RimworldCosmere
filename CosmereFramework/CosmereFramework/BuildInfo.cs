a7a51f35feb7d56c4665188ea3e95010ba92e4b8
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-09 22:25:57 -0500

    Adding Seeker and Coppercloud

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "11fe405";
+        public const string REVISION = "b83afdd";
 
-        public const string BUILD_TIME = "2025-06-09T19:57:08Z";
+        public const string BUILD_TIME = "2025-06-10T03:21:10Z";
     }
 }

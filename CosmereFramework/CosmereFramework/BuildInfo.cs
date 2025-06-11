7c1fb5efbb0a3ecfcb0bb6a01bc097bffd97cb8c
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-11 15:31:38 -0500

    Finishing up duralumin!

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "DEV";
-        
-        public const string BUILD_TIME = "2025-06-11T07:41:20Z";
+        public const string REVISION = "dd5d951";
+
+        public const string BUILD_TIME = "2025-06-11T20:26:25Z";
     }
 }

cdca45ac9d009f3fc37a418b4c0c8e16f4b3f97f
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-11 22:14:18 -0500

    Fixing Nicrosil ability type. Still needs to wipe target's metal reserve for burning metals

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "bcaf2b3";
+        public const string REVISION = "4d025f4";
 
-        public const string BUILD_TIME = "2025-06-12T03:03:26Z";
+        public const string BUILD_TIME = "2025-06-12T03:10:59Z";
     }
 }

26a0d92bd06bd68f23caf58f13c2b35b9651880d
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-15 23:50:20 -0500

    Fixes for 1.6

--- CosmereScadrial/CosmereScadrial/Abilities/Allomancy/Hediffs/InvestitureShieldHediff.cs
+++ CosmereScadrial/CosmereScadrial/Abilities/Allomancy/Hediffs/InvestitureShieldHediff.cs
@@ -12,18 +12,20 @@ namespace CosmereScadrial.Abilities.Allomancy.Hediffs {
         public override void Tick() {
             base.Tick();
 
-            var metal = sourceAbilities.First().metal;
+            var metal = sourceAbilities.FirstOrDefault()?.metal;
+            if (metal == null) return;
+
             var metalsToWipe = reserves.GetAllAvailableMetals().ToList();
             if (metal == MetallicArtsMetalDefOf.Aluminum) {
                 metalsToWipe.Remove(metal);
             }
 
             if (metalsToWipe.Count == 0) return;
-            
+
             foreach (var metalToWipe in metalsToWipe) {
                 reserves.RemoveReserve(metalToWipe);
             }
-            
+
             Messages.Message(
                 GetMessage(metal, metalsToWipe),
                 MessageTypeDefOf.NeutralEvent,
@@ -33,25 +35,26 @@ namespace CosmereScadrial.Abilities.Allomancy.Hediffs {
 
         private string GetMessage(MetallicArtsMetalDef metal, List<MetallicArtsMetalDef> metalsToWipe) {
             var metalsToWipeString = FormatDefList(metalsToWipe);
-            if (metal.Equals(MetallicArtsMetalDefOf.Aluminum))
+            if (metal.Equals(MetallicArtsMetalDefOf.Aluminum)) {
                 return
                     $"{pawn.NameFullColored} is burning {metal.LabelCap} and has wiped his reserves of {metalsToWipeString}.";
+            }
 
-            if (metal.Equals(MetallicArtsMetalDefOf.Chromium))
+            if (metal.Equals(MetallicArtsMetalDefOf.Chromium)) {
                 return
                     $"{pawn.NameFullColored} is the target of a lurcher, and is losing all of their reserves of {metalsToWipeString}.";
+            }
 
             return "";
         }
-        public static string FormatDefList(List<MetallicArtsMetalDef> defs)
-        {
+
+        public static string FormatDefList(List<MetallicArtsMetalDef> defs) {
             var names = defs.Select(m => m.LabelCap).ToList();
-            return names.Count switch
-            {
+            return names.Count switch {
                 0 => "",
                 1 => names[0],
                 2 => $"{names[0]} and {names[1]}",
-                _ => string.Join(", ", names.Take(names.Count - 1)) + " and " + names.Last()
+                _ => string.Join(", ", names.Take(names.Count - 1)) + " and " + names.Last(),
             };
         }
     }

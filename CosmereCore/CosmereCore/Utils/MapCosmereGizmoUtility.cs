50efdd5ade0f1fbee487104e8ee28ecda9382427
Aaron Scherer <aequasi@gmail.com>
2025-06-18 07:28:23 -0500

    More work on the new ability design

---
+++ CosmereCore/CosmereCore/Utils/MapCosmereGizmoUtility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace CosmereCore.Utils {
+    [StaticConstructorOnStartup]
+    public static class MapCosmereGizmoUtility {
+        private static Gizmo mouseoverGizmo;
+        private static Gizmo lastMouseOverGizmo;
+        private static int cacheFrame;
+        private static readonly List<object> tmpObjectsList = new List<object>();
+
+        public static Gizmo LastMouseOverGizmo => MapCosmereGizmoUtility.lastMouseOverGizmo;
+
+        public static void MapUIOnGUI()
+        {
+            if (Find.MainTabsRoot.OpenTab != null && Find.MainTabsRoot.OpenTab != MainButtonDefOf.Inspect)
+                return;
+            tmpObjectsList.Clear();
+            tmpObjectsList.AddRange(Find.Selector.SelectedObjects);
+            GizmoGridDrawer.DrawGizmoGridFor(tmpObjectsList, out mouseoverGizmo);
+        }
+
+        public static void MapUIUpdate()
+        {
+            lastMouseOverGizmo = mouseoverGizmo;
+            mouseoverGizmo?.GizmoUpdateOnMouseover();
+        }
+    }
+}
+
--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string REVISION = "899a4f5";
-
-        public const string BUILD_TIME = "2025-06-18T02:10:08Z";
+        public const string REVISION = "94162e2";
+        
+        public const string BUILD_TIME = "2025-06-18T05:19:11Z";
     }
 }

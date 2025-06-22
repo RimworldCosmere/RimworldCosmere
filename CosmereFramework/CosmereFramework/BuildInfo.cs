f604d98a052a1e50c19f08231036791ad6e0f903
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-21 22:23:27 -0500

    Removing investiture tab

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string Revision = "b54de4a";
+        public const string Revision = "a501dfe";
 
-        public const string BuildTime = "2025-06-21T20:14:40Z";
+        public const string BuildTime = "2025-06-22T03:14:32Z";
     }
 }

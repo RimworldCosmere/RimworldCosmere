ee42a4beb5cc8666676daac66fe3a3f5fd2afe67
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-22 17:18:43 -0500

    Fixing metal burn rate at night

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string Revision = "f5f5f27";
+        public const string Revision = "41d0c7c";
 
-        public const string BuildTime = "2025-06-22T21:50:55Z";
+        public const string BuildTime = "2025-06-22T22:15:06Z";
     }
 }

cda730933162bdc798525e8b23992fe8928908e7
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-25 13:39:11 -0500

    Starting work on atium caravan

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string Revision = "131bb37";
+        public const string Revision = "c46ab48";
 
-        public const string BuildTime = "2025-06-25T17:36:01Z";
+        public const string BuildTime = "2025-06-25T18:29:10Z";
     }
 }
---
+++ CosmereScadrial/CosmereScadrial/IncidentWorker/AtiumCaravan.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using CosmereResources;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+namespace CosmereScadrial.IncidentWorker;
+
+public class AtiumCaravan : RimWorld.IncidentWorker {
+    protected override bool CanFireNowSub(IncidentParms parms) {
+        return base.CanFireNowSub(parms) && Find.CurrentMap != null;
+    }
+
+    protected override bool TryExecuteWorker(IncidentParms parms) {
+        Map map = (Map)parms.target;
+
+        // Find spawn and exit points
+        if (!RCellFinder.TryFindRandomPawnEntryCell(out IntVec3 entryPoint, map, CellFinder.EdgeRoadChance_Neutral)) {
+            return false;
+        }
+
+        IntVec3 exitPoint = CellFinder.RandomEdgeCell(map);
+
+        // @TODO Create new faction
+        Faction faction = Find.FactionManager.OfAncientsHostile;
+        PawnGroupMakerParms groupParms = new PawnGroupMakerParms {
+            groupKind = PawnGroupKindDefOf.Combat,
+            tile = map.Tile,
+            faction = faction,
+            points = 300f,
+            raidStrategy = RaidStrategyDefOf.ImmediateAttack,
+        };
+
+        List<Pawn> attackers = PawnGroupMakerUtility.GeneratePawns(groupParms).ToList();
+
+        if (attackers.Count == 0) {
+            return false;
+        }
+
+        // Give a few pawns Atium
+        foreach (Pawn? p in attackers.Take(2)) // First two
+        {
+            Verse.Thing atium = ThingMaker.MakeThing(CosmereResources.ThingDefOf.Atium);
+            atium.stackCount = Rand.RangeInclusive(1, 3);
+            p.inventory?.TryAddItemNotForSale(atium);
+        }
+
+        // Spawn and start raid
+        foreach (Pawn? pawn in attackers) {
+            GenSpawn.Spawn(pawn, CellFinder.RandomClosewalkCellNear(entryPoint, map, 10), map);
+        }
+
+        LordJob lordJob = new LordJob_ExitMapNear(exitPoint, LocomotionUrgency.Walk, 6f, false, true);
+        LordMaker.MakeNewLord(faction, lordJob, map, attackers);
+
+        string coloredMetal = MetalDefOf.Atium.coloredLabel;
+        Find.LetterStack.ReceiveLetter(
+            "Atium Caravan",
+            "CS_MetalCaravanLetter".Translate(MetalDefOf.Atium.coloredLabel.Named("METAL")).Resolve(),
+            LetterDefOf.PositiveEvent,
+            new TargetInfo(entryPoint, map)
+        );
+
+        return true;
+    }
+}

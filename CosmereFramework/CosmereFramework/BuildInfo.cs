2a5484b58f64223407a7946fb202b7007590cef1
Aaron Scherer <aequasi@gmail.com>
2025-06-26 12:36:24 -0500

    Iron Pulling and Steel Pushing will now hurt pawns that the item goes through

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string Revision = "9e2929e";
+        public const string Revision = "e9b7f8b";
         
-        public const string BuildTime = "2025-06-26T04:50:41Z";
+        public const string BuildTime = "2025-06-26T17:18:56Z";
     }
 }

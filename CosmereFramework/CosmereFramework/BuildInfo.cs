f505c1c1d6a1b5bbc03e154a47cb4395b9ae1dfb
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-29 23:24:16 -0500

    Trying to automatically handle versions now

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string Revision = "dc6f7a8";
+        public const string Revision = "ebf73a4";
 
-        public const string BuildTime = "2025-06-30T03:53:16Z";
+        public const string BuildTime = "2025-06-30T04:20:35Z";
     }
 }

8084f62b7bed85ae80f1d688993d8b2f280d8c95
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-06-29 23:46:45 -0500

    Testing aseembly info again

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework {
     public static class BuildInfo {
-        public const string Revision = "ebf73a4";
+        public const string Revision = "d70eeb0";
 
-        public const string BuildTime = "2025-06-30T04:20:35Z";
+        public const string BuildTime = "2025-06-30T04:44:44Z";
     }
 }

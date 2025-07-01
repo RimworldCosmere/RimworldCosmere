f6f58ae6b528ac750c358a1d9ba622ed64113286
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-07-01 17:14:31 -0500

    Patching bad research window method

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++ CosmereFramework/CosmereFramework/BuildInfo.cs
@@ -1,7 +1,7 @@
 namespace CosmereFramework;
 
 public static class BuildInfo {
-    public const string Revision = "162ab99";
+    public const string Revision = "9ef0d38";
 
-    public const string BuildTime = "2025-07-01T21:58:22Z";
+    public const string BuildTime = "2025-07-01T22:11:53Z";
 }

584feff5bbca8fad6e93d5922b27951899bf2bc9
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-07-01 00:46:35 -0500

    Removing BuildInfo from git

--- CosmereFramework/CosmereFramework/BuildInfo.cs
+++
@@ -1,7 +0,0 @@
-namespace CosmereFramework {
-    public static class BuildInfo {
-        public const string Revision = "17192ed";
-
-        public const string BuildTime = "2025-07-01T04:38:07Z";
-    }
-}

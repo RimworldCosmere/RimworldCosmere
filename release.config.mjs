2e8038360ed97f6ae69db8ea7379e5e59f2ac5e8
Aaron Scherer <aequasi@gmail.com>
2025-07-03 02:26:02 -0500

    fix: Adding semantic-release

--- release.config.mjs
+++ release.config.mjs
@@ -3,5 +3,17 @@
  */
 export default {
     branches: ["main", {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github",
+            {
+                "assets": ["**/Assemblies"]
+            }
+        ],
+        "@semantic-release/git",
+        "@semantic-release/changelog"
+    ],
     tagFormat: "${version}",
 };

91c34fd8b2df1b23db5923bae9dd94ad7f4a7fd5
Aaron Scherer <aequasi@gmail.com>
2025-07-03 02:21:10 -0500

    fix: Adding semantic-release

--- release.config.mjs
+++ release.config.mjs
@@ -3,6 +3,5 @@
  */
 export default {
     branches: ["main", {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
-    repositoryUrl: 'https://github.com/RimworldCosmere/RimworldCosmere/',
     tagFormat: "${version}",
 };

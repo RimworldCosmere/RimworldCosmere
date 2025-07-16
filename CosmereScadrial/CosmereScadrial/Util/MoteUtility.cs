13d0143afe1bdf54765d68a9ca4c388eb1ebe79c
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-07-16 16:36:33 -0500

    fix: Updating shaders for Brass, Copper, Zinc

--- CosmereScadrial/CosmereScadrial/Util/MoteUtility.cs
+++ CosmereScadrial/CosmereScadrial/Util/MoteUtility.cs
@@ -4,10 +4,6 @@ namespace CosmereScadrial.Util;
 
 public static class MoteUtility {
     public static float GetMoteSize(ThingDef moteDef, float radius, float multiplier) {
-        float desiredRadius = radius * multiplier; // includes flare doubling if needed
-        float drawSize = moteDef.graphicData.drawSize.magnitude; // assume square; use magnitude if not
-        float scale = desiredRadius * 2f * drawSize; // times 2 since radius = diameter / 2
-
-        return scale + multiplier * drawSize;
+        return radius * multiplier * 2f * moteDef.graphicData.drawSize.normalized.magnitude;
     }
 }

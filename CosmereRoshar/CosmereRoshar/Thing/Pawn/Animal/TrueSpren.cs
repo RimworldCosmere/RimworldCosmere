5f7624b036a5decb522bb431b94c953bb390fbaa
Aaron Scherer <896295+aequasi@users.noreply.github.com>
2025-08-04 10:02:29 -0500

    Forgot to destroy the spren after bonding

--- CosmereRoshar/CosmereRoshar/Thing/Pawn/Animal/TrueSpren.cs
+++ CosmereRoshar/CosmereRoshar/Thing/Pawn/Animal/TrueSpren.cs
@@ -12,6 +12,10 @@ public class TrueSpren : Spren {
     public override void Notify_SignalReceived(Signal signal) {
         base.Notify_SignalReceived(signal);
         if (signal.tag is not SpiritWeb.CHANGED_SIGNAL) return;
+        CheckForBond();
+    }
+
+    private void CheckForBond() {
         if (bond == null || connectionWithBond == null) return;
         if (connectionWithBond.value < 1.0) return;
 
@@ -27,10 +31,12 @@ public class TrueSpren : Spren {
             bond,
             MessageTypeDefOf.PositiveEvent
         );
+        Destroy();
     }
 
     protected override void TickInterval(int delta) {
         base.TickInterval(delta);
         if (!this.IsHashIntervalTick(GenTicks.TickLongInterval, delta)) return;
+        CheckForBond();
     }
 }

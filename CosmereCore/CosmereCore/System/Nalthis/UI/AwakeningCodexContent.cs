91ec135dfc8d76c1609070fd72a1a53a2e07babd
Aaron Scherer <aequasi@gmail.com>
2026-04-21 16:44:34 -0500

    feat(nalthis): add Awakening Codex placeholder content

---
+++ CosmereCore/CosmereCore/System/Nalthis/UI/AwakeningCodexContent.cs
@@ -0,0 +1,23 @@
+using Cosmere.Core.UI.Codex;
+using UnityEngine;
+using Verse;
+
+namespace Cosmere.System.Nalthis.UI;
+
+public sealed class AwakeningCodexContent : ICodexContentProvider {
+    public bool HasProgression(Pawn pawn) => true;
+    public bool HasBonded(Pawn pawn) => false;
+    public bool HasMemories(Pawn pawn) => false;
+    public string? HeaderLabelFor(Pawn pawn) => null;
+
+    public void DrawProgression(Pawn pawn, Rect rect) {
+        using (new TextBlock(GameFont.Medium, TextAnchor.MiddleLeft, Color.white))
+            Widgets.Label(new Rect(rect.x, rect.y, rect.width, 30f), "CC_Codex_Awakening_Progression_Header".Translate());
+
+        using (new TextBlock(GameFont.Small, TextAnchor.MiddleLeft, new Color(0.75f, 0.75f, 0.75f)))
+            Widgets.Label(new Rect(rect.x, rect.y + 34f, rect.width, 24f), "CC_Codex_Awakening_Progression_Placeholder".Translate());
+    }
+
+    public void DrawBonded(Pawn pawn, Rect rect) { }
+    public void DrawMemories(Pawn pawn, Rect rect) { }
+}
--- CosmereCore/CosmereCore/System/Nalthis/UI/AwakeningInvestitureProvider.cs
+++ CosmereCore/CosmereCore/System/Nalthis/UI/AwakeningInvestitureProvider.cs
@@ -1,11 +1,14 @@
+using Cosmere.Core.UI.Codex;
 using Cosmere.Core.UI.Model;
 using Cosmere.Core.UI.Radial;
 using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace Cosmere.System.Nalthis.UI;
 
-public sealed class AwakeningInvestitureProvider : IInvestitureProvider {
+public sealed class AwakeningInvestitureProvider : IInvestitureProvider, ICodexContentProvider {
+    private static readonly AwakeningCodexContent codex = new();
     public string SystemId => "Awakening";
 
     public bool IsInvested(Pawn pawn) {
@@ -19,4 +22,12 @@ public sealed class AwakeningInvestitureProvider : IInvestitureProvider {
     public RadialSystem? SnapshotRadial(Pawn pawn) {
         return null;
     }
+
+    public bool HasProgression(Pawn pawn) => codex.HasProgression(pawn);
+    public void DrawProgression(Pawn pawn, Rect rect) => codex.DrawProgression(pawn, rect);
+    public bool HasBonded(Pawn pawn) => codex.HasBonded(pawn);
+    public void DrawBonded(Pawn pawn, Rect rect) => codex.DrawBonded(pawn, rect);
+    public bool HasMemories(Pawn pawn) => codex.HasMemories(pawn);
+    public void DrawMemories(Pawn pawn, Rect rect) => codex.DrawMemories(pawn, rect);
+    public string? HeaderLabelFor(Pawn pawn) => codex.HeaderLabelFor(pawn);
 }

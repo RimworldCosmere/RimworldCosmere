c75e2d3c4a9adb1f997e71daac44dd123378aca8
Aaron Scherer <aequasi@gmail.com>
2026-04-22 02:03:19 -0500

    feat(lightweave): EdgeInsets / RadiusSpec / BorderSpec / BackgroundSpec

---
+++ CosmereCore/CosmereCore/Core/UI/Lightweave/Types/EdgeInsets.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Cosmere.Core.UI.Lightweave.Types;
+
+public readonly record struct EdgeInsets(
+    Rem? Top = null,
+    Rem? Right = null,
+    Rem? Bottom = null,
+    Rem? Left = null,
+    Rem? Start = null,
+    Rem? End = null)
+{
+    public static readonly EdgeInsets Zero = new EdgeInsets();
+
+    public static EdgeInsets All(Rem v) => new EdgeInsets(v, v, v, v);
+    public static EdgeInsets Horizontal(Rem v) => new EdgeInsets(Left: v, Right: v);
+    public static EdgeInsets Vertical(Rem v) => new EdgeInsets(Top: v, Bottom: v);
+    public static EdgeInsets FromStart(Rem v) => new EdgeInsets(Start: v);
+    public static EdgeInsets FromEnd(Rem v) => new EdgeInsets(End: v);
+
+    public (float l, float t, float r, float b) Resolve(Direction dir)
+    {
+        float startPx = Start?.ToPixels() ?? 0f;
+        float endPx = End?.ToPixels() ?? 0f;
+        float leftPx = (Left?.ToPixels() ?? 0f) + (dir == Direction.Ltr ? startPx : endPx);
+        float rightPx = (Right?.ToPixels() ?? 0f) + (dir == Direction.Ltr ? endPx : startPx);
+        return (leftPx, Top?.ToPixels() ?? 0f, rightPx, Bottom?.ToPixels() ?? 0f);
+    }
+
+    public Rect Shrink(Rect r, Direction dir)
+    {
+        (float l, float t, float rt, float b) = Resolve(dir);
+        return new Rect(r.x + l, r.y + t, r.width - l - rt, r.height - t - b);
+    }
+}

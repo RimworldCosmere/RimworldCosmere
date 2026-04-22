f2a2258645112ba2ccbeb8903a6ac47c32a8924d
Aaron Scherer <aequasi@gmail.com>
2026-04-22 11:16:57 -0500

    feat(lightweave): Layout.Stack (absolute z-layered children)

---
+++ CosmereCore/CosmereCore/Core/UI/Lightweave/Layout/Stack.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Cosmere.Core.UI.Lightweave.Runtime;
+
+namespace Cosmere.Core.UI.Lightweave.Layout;
+
+public static partial class Layout
+{
+    public static LightweaveNode Stack(
+        Action<List<LightweaveNode>>? children = null,
+        [CallerLineNumber] int line = 0,
+        [CallerFilePath] string file = "")
+    {
+        List<LightweaveNode> kids = new List<LightweaveNode>();
+        children?.Invoke(kids);
+        LightweaveNode node = NodeBuilder.New("Stack", line, file);
+        node.Children.AddRange(kids);
+        node.Paint = rect =>
+        {
+            foreach (LightweaveNode child in kids)
+            {
+                child.MeasuredRect = rect;
+            }
+        };
+        return node;
+    }
+}

ff0609f2a708ff97dba9a136aa882cc7d2a4ddaf
Aaron Scherer <aequasi@gmail.com>
2026-04-22 11:07:46 -0500

    feat(lightweave): NodeBuilder with [CallerLineNumber]+[CallerFilePath] identity

---
+++ CosmereCore/CosmereCore/Core/UI/Lightweave/Runtime/NodeBuilder.cs
@@ -0,0 +1,24 @@
+using System.Runtime.CompilerServices;
+
+namespace Cosmere.Core.UI.Lightweave.Runtime;
+
+public static class NodeBuilder
+{
+    public static LightweaveNode New(
+        string debugName,
+        [CallerLineNumber] int line = 0,
+        [CallerFilePath] string file = "")
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 31 + (file?.GetHashCode() ?? 0);
+            hash = hash * 31 + line;
+            return new LightweaveNode
+            {
+                CallSiteId = hash,
+                DebugName = debugName,
+            };
+        }
+    }
+}

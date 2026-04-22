83a6e5c263405ca644d475c05fb5e7ad0ee0aa82
Aaron Scherer <aequasi@gmail.com>
2026-04-22 02:04:12 -0500

    feat(lightweave): RadiusScale / ElevationScale / SpacingScale

---
+++ CosmereCore/CosmereCore/Core/UI/Lightweave/Tokens/ElevationScale.cs
@@ -0,0 +1,9 @@
+namespace Cosmere.Core.UI.Lightweave.Tokens;
+
+public enum ElevationScale
+{
+    Flat,
+    Sm,
+    Md,
+    Lg,
+}
---
+++ CosmereCore/CosmereCore/Core/UI/Lightweave/Tokens/RadiusScale.cs
@@ -0,0 +1,10 @@
+namespace Cosmere.Core.UI.Lightweave.Tokens;
+
+public enum RadiusScale
+{
+    None,
+    Sm,
+    Md,
+    Lg,
+    Full,
+}
